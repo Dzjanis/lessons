3131fd5f7e1b63736790c6646e0580697e0963cb
Dzjanis <Roah.thaar.g@gmail.com>
2025-04-08 22:54:33 +0300

    direction add slider

--- js/script.js
+++ js/script.js
@@ -1,36 +1,50 @@
-    // Находим контейнер со слайдами
-    const slides = document.querySelector('.impressions__cards');
+/*direction_slider*/
+const slides1 = document.querySelector('.directions__cards');
+const slideCount1 = document.querySelectorAll('.directions__cards_item').length;
+const prevButton1 = document.querySelector('.directions__bck');
+const nextButton1 = document.querySelector('.directions__frw');
 
-    // Определяем количество слайдов
-    const slideCount = document.querySelectorAll('.impressions__cards_item').length;
+let currentIndex1 = 0;
 
-    // Находим кнопки «Назад» и «Вперёд»
-    const prevButton = document.querySelector('.impressions__bck');
-    const nextButton = document.querySelector('.impressions__frw');
-
-    let currentIndex = 0; // Переменная для хранения текущего слайда
-
-    // Функция смены слайдов
-    function goToSlide(index) {
-    if (index < 0) {
-    index = slideCount - 1; // Если нажали «Назад» на первом слайде, переходим на последний
-} else if (index >= slideCount) {
-    index = 0; // Если нажали «Вперёд» на последнем слайде, переходим на первый
+function goToSlide1(index) {
+  if (index < 0) {
+    index = slideCount1 - 1;
+  } else if (index >= slideCount1) {
+    index = 0;
+  }
+  currentIndex1 = index;
+  slides1.style.transform = `translateX(calc(${-index * 100}vw + ${index * 24}px))`;
+  console.log(currentIndex1);
 }
 
-    currentIndex = index; // Запоминаем текущий слайд
-    slides.style.transform = `translateX(calc(${-index * 100}vw + ${index * 16}px))`; // Сдвигаем контейнер со слайдами
-    console.log(currentIndex);
+prevButton1.addEventListener('click', () => {
+  goToSlide1(currentIndex1 - 1);
+});
+nextButton1.addEventListener('click', () => {
+  goToSlide1(currentIndex1 + 1);
+});
+/*impression_slider*/
+const slides3 = document.querySelector('.impressions__cards');
+const slideCount3 = document.querySelectorAll('.impressions__cards_item').length;
+const prevButton3 = document.querySelector('.impressions__bck');
+const nextButton3 = document.querySelector('.impressions__frw');
+
+let currentIndex3 = 0;
+
+function goToSlide3(index) {
+  if (index < 0) {
+    index = slideCount3 - 1;
+  } else if (index >= slideCount3) {
+    index = 0;
+  }
+  currentIndex3 = index;
+  slides3.style.transform = `translateX(calc(${-index * 100}vw + ${index * 16}px))`;
+  console.log(currentIndex3);
 }
 
-    // Добавляем обработчик клика для кнопки «Назад»
-    prevButton.addEventListener('click', () => {
-    goToSlide(currentIndex - 1);
+prevButton3.addEventListener('click', () => {
+  goToSlide3(currentIndex3 - 1);
 });
-
-    // Добавляем обработчик клика для кнопки «Вперёд»
-    nextButton.addEventListener('click', () => {
-    goToSlide(currentIndex + 1);
+nextButton3.addEventListener('click', () => {
+  goToSlide3(currentIndex3 + 1);
 });
-
-    // Устанавливаем первый активный слайд при загрузке страницы
